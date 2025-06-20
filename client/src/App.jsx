0c07bdfa4f2080c02ee6ffb0f910e200c5e1e427
developermanash <manashpratim84866@gmail.com>
2025-06-20 18:57:34 +0530

    created signup page and connected with the backend using TSQ

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import Login from "./pages/Login.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import { axiosInstance } from './lib/axios.js';
+import { useQuery } from '@tanstack/react-query';
 
-function App() {
-  const [count, setCount] = useState(0)
+import toast, { Toaster } from "react-hot-toast"
+import axios from 'axios';
 
+const App = () => {
+  const { data: authData, isLoading, error } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+  const authUser = authData?.user
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <Routes>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!authUser ? <Login /> : <Navigate to="/" />} />
+        <Route path='/notification' element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path='/onboarding' element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
+        <Route path='/chat' element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path='/call' element={authUser ? <CallPage /> : <Navigate to="/login" />} />
+
+      </Routes>
+      <Toaster />
+    </div>
   )
 }
 
-export default App
+export default
+  App
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import chatRoutes from "./routes/chat.routes.js"
+import cors from 'cors';
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import chatRoutes from "./routes/chat.routes.js";
 import { connectDB } from "./lib/db.js"; 
 import cookieParser from "cookie-parser";
 
@@ -11,6 +12,11 @@ dotenv.config();
 
 const app = express();
 const  PORT = process.env.PORT || 8000
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true //allow frontend to send the cookies
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
